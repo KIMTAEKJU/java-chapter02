0ca7194e39f8350b9749f19713d49a588e7f8308
bit <bit@DESKTOP-0G705V6>
2018-12-28 19:47:13 +0900

    문제 해결

--- src/chapter02/ArrayUtil.java
+++ src/chapter02/ArrayUtil.java
@@ -2,42 +2,42 @@ package chapter02;
 
 public class ArrayUtil {
 	// int 배열을 double 배열로 변환
-	public static double[] intToDouble( int[] source ) {
-		double[] result = new double[source.length];
-		
-		for(int i = 0; i < source.length; i++) {
-			result[i] = source[i];
+		public static double [] intToDouble( int[] source )
+		{
+			double[] doubleArr = new double[source.length];
+			
+			for (int i = 0; i < source.length; i++)
+				doubleArr[i] = (double)source[i];
+			
+			return doubleArr;
 		}
 		
-		return result;
-	}
-	
-	// double 배열을 int 배열로 변환
-	public static int[] doubleToInt( double[] source ) {
-		int[] result = new int[source.length];
-		
-		for(int i = 0; i < source.length; i++) {
-			result[i] = (int)source[i];
+		// double 배열을 int 배열로 변환
+		public static int [] doubleToInt( double[] source )
+		{
+			int[] intArr = new int[source.length];
+			
+			for (int i = 0; i < source.length; i++)
+				intArr[i] = (int)source[i];
+			
+			return intArr;
 		}
-		
-		return result;
-	}
-	
-	// int 배열 두 개를 연결한 새로운 배열 리턴
-	public static int[] concat( int[] s1, int[] s2 ) {
-		int[] result = new int[s1.length + s2.length];
-		
-		int index = 0;
-		
-		for(int val : s1) {
-			result[index++] = val;
+		// int 배열 두 개를 연결한 새로운 배열 리턴
+		public static int [] concat( int[] s1, int[] s2 )
+		{
+			int[] combinationArray = new int[s1.length + s2.length];
+			int index = 0;
+			
+			for(int i : s1)
+				combinationArray[index++] = i;
+			
+			
+			for(int i : s2)
+				combinationArray[index++] = i;
+			
+			
+			
+			return combinationArray;
 		}
-
-		for(int val : s2) {
-			result[index++] = val;
-		}
-		
-		return result;
-	}
 
 }
