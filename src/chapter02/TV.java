75c7026e69a256a53da1784dc9261895c69e08bc
bit <bit@DESKTOP-0G705V6>
2018-12-28 19:47:51 +0900

    문제 해결

--- src/chapter02/TV.java
+++ src/chapter02/TV.java
@@ -2,63 +2,98 @@ package chapter02;
 
 public class TV {
 	private boolean power;
-	private int channel;  //0~50
-	private int volume;   //0~50
+	private int channel; // 0 ~ 50번까지
+	private int volume; // 0 ~ 50 까지
 	
-	public TV(int channel, int volume, boolean power) {
-		this.channel = channel;
-		this.volume = volume;
+	public TV (int channel, int volume, boolean power)
+	{
+		power(power);
+		channel(channel);
+		volume(volume);
+	}
+	
+	public void overValueCalc(int value, boolean check)
+	{
+		if (check)
+		{
+			if (value < 0)
+				this.volume = 50;
+			
+			else if (value > 50)
+				this.volume = 0;
+			
+			else
+			{
+				while(true)
+				{
+					if (value > this.volume)
+						volume(true);
+					else if (value < this.volume)
+						volume(false);
+					else
+						break;
+				}
+			}
+		}
+		
+		else
+		{
+			if (value < 0)
+				this.channel = 50;
+			
+			else if (value > 50)
+				this.channel = 0;
+			
+			else
+			{
+				while(true)
+				{
+					if (value > this.channel)
+						channel(true);
+					else if (value < this.channel)
+						channel(false);
+					else
+						break;
+				}
+			}
+		}
+	}
+	
+	public void volume(int volume)
+	{
+		overValueCalc(volume, true);
+	}
+	
+	public void volume(boolean volume)
+	{
+		if (volume)
+			overValueCalc(++this.volume, true);
+		else
+			overValueCalc(--this.volume, true);
+	}
+	
+	public void power(boolean power)
+	{
 		this.power = power;
 	}
 	
-	public void power(boolean on) {
-		power = on;
+	public void channel(int channel)
+	{
+		overValueCalc(channel, false);
 	}
 	
-	public void channel(int channel) {
-		if( !power ) {
-			return;
-		}
-		
-		if( channel > 50 ) {
-			channel = 0;
-		}
-		
-		if( channel < 0 ) {
-			channel = 50;
-		}
-
-		this.channel = channel;
+	public void channel(boolean up)
+	{
+		if (up)
+			overValueCalc(++this.channel, false);
+		else
+			overValueCalc(--this.channel, false);
 	}
 	
-	public void channel(boolean up) {
-		channel(channel + (up ? 1 : -1));
-	}
-
-	public void volume(int volume) {
-		if( !power ) {
-			return;
-		}
-		
-		if( volume > 50 ) {
-			volume = 0;
-		}
-		
-		if( volume < 0 ) {
-			volume = 50;
-		}
-
-		this.volume = volume;
-	}
-	
-	public void volume(boolean up) {
-		channel(volume + (up ? 1 : -1));
-	}
-	
-	public void status() {
-		System.out.println(
-			"TV[power="  + power   + 
-			", channel=" + channel + 
-			", volume="  + volume  + "]");
+	public void status()
+	{
+		System.out.println("TV[power=" + power +
+						",channel=" + channel + 
+						", volume=" + volume + "]");
 	}
 }
